67aa2037c2ec4629e7a7aee3ac3b581b2662a65e
Wilbert <wilbertfu.wf@gmail.com>
2023-05-12 08:29:46 +0700

    authentication implemented 2

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@
 require('dotenv').config();
  
 const Hapi = require('@hapi/hapi');
- 
+const Jwt = require('@hapi/jwt') ;
+
 // notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
@@ -34,6 +35,28 @@ const init = async () => {
     },
   });
  
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('notesapp_jwt','jwt',{
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify:{
+      aud:false,
+      iss:false,
+      sub:false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate:(artifacts)=>({
+      isValid:true,
+      credentials:{
+        id:artifacts.decoded.payload.id,
+      },
+    }),
+  });
+
   await server.register([
     {
       plugin: notes,
